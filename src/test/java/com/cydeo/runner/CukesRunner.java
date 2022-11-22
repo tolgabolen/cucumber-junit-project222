cbaacb642449f587c8428019d1de617686d90572
tolga bolen <tolgabolen@hotmail.com>
2022-11-22 17:24:43 -0500

    json plug in added

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -8,7 +8,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = {"html:target/cucumber-report.html",
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         } ,
